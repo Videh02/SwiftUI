53608c02764fbd714379e4295bd1d8fef76fb611
Khanna, Videh Rakesh Rakesh <videkhanna@deloitte.com>
2020-07-20 14:18:38 +0530

    segment view

--- ListSwift/ListSwift/ContentView.swift
+++ ListSwift/ListSwift/ContentView.swift
@@ -5,7 +5,34 @@ struct Pokemon {
     let color: Color
 }
 
-struct ContentView : View {
+struct ContentView: View {
+    @State private var selectedIndex = 0
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 10) {
+                Picker("", selection: $selectedIndex) {
+                    Text("Pokemon").tag(0)
+                    Text("Evolution").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                if selectedIndex == .zero {
+                    ListView()
+                } else {
+                    ScrollContentView()
+                }
+            }
+        }.navigationBarTitle("Pokemon's")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ListView: View {
     var pokemonList = [
         Pokemon(name: "Charmander", type: "Fire", color: .red),
         Pokemon(name: "Squirtle", type: "Water", color: .blue),
@@ -13,10 +40,10 @@ struct ContentView : View {
         Pokemon(name: "Pikachu", type: "Electric", color: .yellow),
     ]
     var body: some View {
-        NavigationView {
-            List(pokemonList, id: \.name) { pokemon in
+        List(pokemonList, id: \.name) { pokemon in
+            NavigationLink(destination: PokemonDetail(pokemon: pokemon)) {
                 CellForRow(cellData: pokemon)
-            }.navigationBarTitle("Pokemon's")
+            }
         }
     }
 }
@@ -28,35 +55,18 @@ struct CellForRow: View {
         HStack(spacing: 10) {
             Text(cellData.name)
             Text(cellData.type).foregroundColor(cellData.color)
-            Spacer()
-            Button(action: self.buttonAction) {
-                Image(systemName: "chevron.right")
-                    .frame(width: 20, height: 20)
-                    .foregroundColor(Color.black)
-            }
         }
-    }
-    
-    private func buttonAction() {
-        let link = NavigationLink(destination: ScrollContentView(),
-                       isActive: $showingAddUser) {
-                        Text("Navigate")
-        }
-        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ListView()
     }
 }
 
 struct ScrollContentView: View {
     var pokemonList = [
-        Pokemon(name: "Charmander", type: "Fire", color: .red),
-        Pokemon(name: "Squirtle", type: "Water", color: .blue),
-        Pokemon(name: "Bulbasaur", type: "Grass", color: .green),
         Pokemon(name: "Pikachu", type: "Electric", color: .yellow),
         Pokemon(name: "Charmelion", type: "Fire", color: .red),
         Pokemon(name: "Wartotle", type: "Water", color: .blue),
@@ -70,7 +80,9 @@ struct ScrollContentView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: true) {
             ForEach(pokemonList, id: \.name) { pokemon in
-                CellForRow(cellData: pokemon)
+                NavigationLink(destination: PokemonDetail(pokemon: pokemon)) {
+                    CellForRow(cellData: pokemon)
+                }
             }
         }
     }
@@ -79,5 +91,15 @@ struct ScrollContentView: View {
 struct ScrollContentView_Previews: PreviewProvider {
     static var previews: some View {
         ScrollContentView()
+    }
+}
+
+struct PokemonDetail: View {
+    let pokemon: Pokemon
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(pokemon.name).foregroundColor(Color.black)
+            Text(pokemon.type).foregroundColor(pokemon.color)
+        }
     }
 }
